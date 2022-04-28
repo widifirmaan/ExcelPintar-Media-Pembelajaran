c511958430e9a54cbfd6c72725ad6a16163376e1
Widi Firmansyah <w@widifirmaan.web.id>
2022-04-28 13:22:55 +0700

    1

---
+++ form/global/config.php
@@ -0,0 +1,15 @@
+<?php
+
+// main program paths - no trailing slashes!
+$g_root_url = "https://excelpintar.web.id/form";
+$g_root_dir = "/home/seet7121/public_html/excelpintar.web.id/form";
+
+// database settings
+$g_db_hostname = "localhost";
+$g_db_port = "3306";
+$g_db_name = "seet7121_ftoo230";
+$g_db_username = "seet7121_ftoo230";
+$g_db_password = "0p@1hS1-Ip";
+$g_table_prefix = "frm3o_";
+
+?>
